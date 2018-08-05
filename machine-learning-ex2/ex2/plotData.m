9cc1216fdf76328aacc33031ea604485d8035d4c
rajatgarg149 <rajatgarg149@gmail.com>
2018-08-05 19:59:05 +0530

    Plotting

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -6,6 +6,14 @@ function plotData(X, y)
 % Create New Figure
 figure; hold on;
 
+%% set x_1: exam1 marks and x_2: exam2 marks; 
+%% x_1 and x_2 as X,Y axis of plots
+X_1 = X(:,1);
+X_2 = X(:,2);
+idx = y==1;
+plot(X_1(idx),X_2(idx),'k+');
+plot(X_1(~idx),X_2(~idx),'ko');
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
