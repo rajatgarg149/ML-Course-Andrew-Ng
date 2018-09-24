48f989fd4d1260bff5b772e8f9887a5c015727eb
rajatgarg149 <rajatgarg149@gmail.com>
2018-09-24 12:29:26 +0530

    Assignment 8 - Complete!

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -17,45 +17,9 @@ J = 0;
 X_grad = zeros(size(X));
 Theta_grad = zeros(size(Theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost function and gradient for collaborative
-%               filtering. Concretely, you should first implement the cost
-%               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
-%               gradient and use the checkCostFunction routine to check
-%               that the gradient is correct. Finally, you should implement
-%               regularization.
-%
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
-%        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
-%
-% You should set the following variables correctly:
-%
-%        X_grad - num_movies x num_features matrix, containing the 
-%                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
-%                     partial derivatives w.r.t. to each element of Theta
-%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% =============================================================
+J = sum(sum(((X * Theta' - Y) .^2) .* R)) / 2 + (lambda / 2) *(sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)));
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
+Theta_grad = (((X * Theta' - Y) .* R)' * X) + lambda * Theta;
 
 grad = [X_grad(:); Theta_grad(:)];
 
--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -11,36 +11,19 @@ bestF1 = 0;
 F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
-for epsilon = min(pval):stepsize:max(pval)
-    
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the F1 score of choosing epsilon as the
-    %               threshold and place the value in F1. The code at the
-    %               end of the loop will compare the F1 score for this
-    %               choice of epsilon and set it to be the best epsilon if
-    %               it is better than the current choice of epsilon.
-    %               
-    % Note: You can use predictions = (pval < epsilon) to get a binary vector
-    %       of 0's and 1's of the outlier predictions
-
-
-
-
-
-
-
-
-
-
-
-
-
-    % =============================================================
-
-    if F1 > bestF1
-       bestF1 = F1;
-       bestEpsilon = epsilon;
-    end
+for epsilon = min(pval):stepsize:max(pval),
+  predictions = (pval < epsilon);
+  tp = sum((predictions == yval)(yval == 1));
+  tn = sum((predictions == yval)(yval == 0));
+  fp = sum((predictions != yval)(yval == 0));
+  fn = sum((predictions != yval)(yval == 1));
+  prec = tp / (tp + fp);
+  rec = tp / (tp + fn);
+  F1 = 2*prec*rec / (prec + rec);
+  if F1 > bestF1
+    bestF1 = F1;
+    bestEpsilon = epsilon;
+  end
 end
 
 end
